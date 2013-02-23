0d89c537e8d21ecdd3aa635ec25ffe99ad9e7374
dianna <diannamertz@gmail.com>
2013-02-23 19:01:40 +0100

    add numbers to circle 13

--- fibonacci_ipad/BackgroundCircle.m
+++ fibonacci_ipad/BackgroundCircle.m
@@ -50,7 +50,7 @@
     tealInnerCircle.fillColor = COLORWHITE;
     tealInnerCircle.strokeColor = COLORTEAL;
     
-    numberCircle8 = [C4Shape ellipse:CGRectMake(0,0,500,500)];
+    numberCircle8 = [C4Shape ellipse:CGRectMake(0,0,525,525)];
     numberCircle8.center = center;
     numberCircle8.fillColor = COLORTRANS;
     numberCircle8.strokeColor = COLORTRANS;
@@ -61,11 +61,11 @@
         number.string = [NSString stringWithFormat:@"%i",i];
         number.alignmentMode = @"center";
         number.fontSize = 25.0;
-        number.foregroundColor = [[UIColor blackColor] CGColor];
+        number.foregroundColor = [[UIColor colorWithRed:RGBToFloat(120) green:RGBToFloat(120) blue:RGBToFloat(120) alpha:RGBToFloat(255)]CGColor];;
         number.bounds = CGRectMake(0.0, 0.0, 25.0, numberCircle8.bounds.size.height / 2.0);
         number.position = CGPointMake(CGRectGetMidX(numberCircle8.bounds ), CGRectGetMidY(numberCircle8.bounds));
         number.anchorPoint = CGPointMake(0.5, 1.0);
-        number.transform = CATransform3DMakeRotation((M_PI * 2) / 8.0 * i, 0, 0, 1);
+        number.transform = CATransform3DMakeRotation(((M_PI * 2) / 8.0 * i)+8, 0, 0, 1);
         [numberCircle8.layer addSublayer:number];
     }
     
@@ -79,10 +79,24 @@
     salmonInnerCircle.fillColor = COLORWHITE;
     salmonInnerCircle.strokeColor = COLORSALMON;
     
-    numberCircle13 = [C4Shape ellipse:CGRectMake(0,0,500,500)];
+    numberCircle13 = [C4Shape ellipse:CGRectMake(0,0,525,525)];
     numberCircle13.center = center;
     numberCircle13.fillColor = COLORTRANS;
     numberCircle13.strokeColor = COLORTRANS;
+    
+    for (NSInteger i=1; i<=13; i++)
+    {
+        CATextLayer *number = [CATextLayer layer];
+        number.string = [NSString stringWithFormat:@"%i",i];
+        number.alignmentMode = @"center";
+        number.fontSize = 25.0;
+        number.foregroundColor = [[UIColor colorWithRed:RGBToFloat(120) green:RGBToFloat(120) blue:RGBToFloat(120) alpha:RGBToFloat(255)]CGColor];;
+        number.bounds = CGRectMake(0.0, 0.0, 30.0, numberCircle13.bounds.size.height / 2.0);
+        number.position = CGPointMake(CGRectGetMidX(numberCircle13.bounds ), CGRectGetMidY(numberCircle13.bounds));
+        number.anchorPoint = CGPointMake(0.5, 1.0);
+        number.transform = CATransform3DMakeRotation(((M_PI * 2) / 13.0 * i)+8, 0, 0, 1);
+        [numberCircle13.layer addSublayer:number];
+    }
     
     [self addShape:whiteCircle];
 }
