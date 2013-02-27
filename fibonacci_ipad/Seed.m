b60eda625713c0be0cbf5f59a3e0ae171174298c
dianna <diannamertz@gmail.com>
2013-02-27 17:43:45 +0100

    add animations

--- fibonacci_ipad/Seed.m
+++ fibonacci_ipad/Seed.m
@@ -12,7 +12,7 @@
 {
     CGPoint seedArcCenter;
     C4Shape *seed, *cone;
-    NSMutableArray *filtered, *filteredSalmonArray0, *filteredSalmonArray1, *filteredSalmonArray2, *filteredSalmonArray3, *filteredSalmonArray4, *filteredSalmonArray5, *filteredSalmonArray6, *filteredSalmonArray7, *filteredSalmonArray8, *filteredSalmonArray9, *filteredSalmonArray10, *filteredSalmonArray11, *filteredSalmonArray12, *filteredTealArray1, *filteredTealArray2, *filteredTealArray3, *filteredTealArray4, *filteredTealArray5, *filteredTealArray6, *filteredTealArray7, *filteredTealArray8;
+    NSMutableArray *filtered, *filtered2, *filteredSalmonArray0, *filteredSalmonArray1, *filteredSalmonArray2, *filteredSalmonArray3, *filteredSalmonArray4, *filteredSalmonArray5, *filteredSalmonArray6, *filteredSalmonArray7, *filteredSalmonArray8, *filteredSalmonArray9, *filteredSalmonArray10, *filteredSalmonArray11, *filteredSalmonArray12, *filteredTealArray1, *filteredTealArray2, *filteredTealArray3, *filteredTealArray4, *filteredTealArray5, *filteredTealArray6, *filteredTealArray7, *filteredTealArray8;
 }
 
 - (id)initWithFrame:(CGRect)frame
@@ -47,6 +47,7 @@
 {
     //seeds
     filtered = [[NSMutableArray alloc] init];
+    filtered2 = [[NSMutableArray alloc] init];
     filteredTealArray1 = [[NSMutableArray alloc] init];
     filteredTealArray2 = [[NSMutableArray alloc] init];
     filteredTealArray3 = [[NSMutableArray alloc] init];
@@ -84,10 +85,14 @@
         //set the color for the seeds
         seed.fillColor = COLORWHITE;
         seed.strokeColor = COLORGREY;
-        
-        if (n) {
+        /*
+        if (n % 2) {
             [filtered addObject:seed];
         }
+        if (!((n) % 2)) {
+            [filtered2 addObject:seed];
+        }
+        */
         if (!((n+0) % 8)) {
             [filteredTealArray1 addObject:seed];
         }
@@ -112,6 +117,7 @@
         if (!((n+7) % 8)) {
             [filteredTealArray8 addObject:seed];
         }
+        
         if (!((n+0) % 13)) {
             [filteredSalmonArray0 addObject:seed];
         }
@@ -164,40 +170,44 @@
 {
     /*
     for (seed in filtered) {
-        seed.animationDuration = 5.0f;
-        seed.fillColor = COLORSALMON;
+        seed.animationDuration = 3.0f;
+        seed.fillColor = COLORWHITE;
+    }
+    for (seed in filtered2) {
+        seed.animationDuration = 3.0f;
+        seed.fillColor = COLORTEAL;
     }
     */
     for (seed in filteredTealArray1) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(0) green:RGBToFloat(174) blue:RGBToFloat(150) alpha:RGBToFloat(255)];
     }
     for (seed in filteredTealArray2) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(31) green:RGBToFloat(182) blue:RGBToFloat(161) alpha:RGBToFloat(255)];
     }
     for (seed in filteredTealArray3) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(62) green:RGBToFloat(190) blue:RGBToFloat(172) alpha:RGBToFloat(255)];
     }
     for (seed in filteredTealArray4) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(93) green:RGBToFloat(198) blue:RGBToFloat(183) alpha:RGBToFloat(255)];
     }
     for (seed in filteredTealArray5) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(124) green:RGBToFloat(206) blue:RGBToFloat(194) alpha:RGBToFloat(255)];
     }
     for (seed in filteredTealArray6) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(155) green:RGBToFloat(214) blue:RGBToFloat(205) alpha:RGBToFloat(255)];
     }
     for (seed in filteredTealArray7) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(186) green:RGBToFloat(222) blue:RGBToFloat(216) alpha:RGBToFloat(255)];
     }
     for (seed in filteredTealArray8) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(218) green:RGBToFloat(231) blue:RGBToFloat(227) alpha:RGBToFloat(255)];
     }
      
@@ -207,67 +217,67 @@
 {
     //8
     for (seed in filteredSalmonArray8) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(246) green:RGBToFloat(145) blue:RGBToFloat(145) alpha:RGBToFloat(255)];
     }
     //13
     for (seed in filteredSalmonArray0) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(242) green:RGBToFloat(80) blue:RGBToFloat(80) alpha:RGBToFloat(255)];
     }
     //5
     for (seed in filteredSalmonArray5) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(249) green:RGBToFloat(184) blue:RGBToFloat(184) alpha:RGBToFloat(255)];
     }
     //10
     for (seed in filteredSalmonArray10) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(244) green:RGBToFloat(119) blue:RGBToFloat(119) alpha:RGBToFloat(255)];
     }
     //2
     for (seed in filteredSalmonArray2) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(252) green:RGBToFloat(223) blue:RGBToFloat(223) alpha:RGBToFloat(255)];
     }
     //7
     for (seed in filteredSalmonArray7) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(247) green:RGBToFloat(158) blue:RGBToFloat(158) alpha:RGBToFloat(255)];
     }
     //12
     for (seed in filteredSalmonArray12) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(242) green:RGBToFloat(93) blue:RGBToFloat(93) alpha:RGBToFloat(255)];
     }
     //4
     for (seed in filteredSalmonArray4) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(250) green:RGBToFloat(197) blue:RGBToFloat(197) alpha:RGBToFloat(255)];
     }
     //9
     for (seed in filteredSalmonArray9) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(245) green:RGBToFloat(132) blue:RGBToFloat(132) alpha:RGBToFloat(255)];
     }
     //1
     for (seed in filteredSalmonArray1) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(253) green:RGBToFloat(237) blue:RGBToFloat(237) alpha:RGBToFloat(255)];
     }
     //6
     for (seed in filteredSalmonArray6) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(248) green:RGBToFloat(171) blue:RGBToFloat(171) alpha:RGBToFloat(255)];
     }
     //11
     for (seed in filteredSalmonArray11) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(243) green:RGBToFloat(106) blue:RGBToFloat(106) alpha:RGBToFloat(255)];
     }
     //3
     for (seed in filteredSalmonArray3) {
-        seed.animationDuration = 5.0f;
+        seed.animationDuration = 3.0f;
         seed.fillColor = [UIColor colorWithRed:RGBToFloat(251) green:RGBToFloat(210) blue:RGBToFloat(210) alpha:RGBToFloat(255)];
     }
 }
